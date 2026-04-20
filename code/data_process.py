9bb161067980141f5c32af82864d3cd24b80a87b
guoyanb <39645139+guoyanb@users.noreply.github.com>
2026-04-20 09:42:53 +0800

    Update data_process.py

--- code/data_process.py
+++ code/data_process.py
@@ -1,6 +1,9 @@
 # [file name]: data_process.py
 import warnings
-from utils import *
+import torch
+import numpy as np
+import pandas as pd
+import os
 from sklearn.model_selection import KFold
 warnings.filterwarnings("ignore")
 
@@ -10,7 +13,6 @@ def data_load():
     gene_feat = pd.read_csv('./Data/gene_sim_BP301.csv', header=None).values
     disease_feat = pd.read_csv('./Data/dis_sim153.txt', header=None, sep='\t').values
 
-    # Ensure feature matrices have correct dimensions
     gene_features = torch.FloatTensor(gene_feat)
     disease_features = torch.FloatTensor(disease_feat)
     microbe_features = torch.FloatTensor(microbe_feat)
@@ -18,13 +20,12 @@ def data_load():
     features = {'g': gene_features, 'm': microbe_features, 'd': disease_features}
     in_size = {'g': gene_features.shape[1], 'm': microbe_features.shape[1],
                'd': disease_features.shape[1]}
-    
+
     print(f"Feature dimensions - Gene: {gene_features.shape}, Microbe: {microbe_features.shape}, Disease: {disease_features.shape}")
-    
+
     return features, in_size
 
 def get_train_val_data(all_data, train_ind, val_ind, adj, seed):
-    """Generate negative samples for training set for 5-fold cross-validation"""
     neg_num_test = 30
     train_data_pos, val_data_pos = all_data[train_ind], all_data[val_ind]
     train_data_all, tr_neg_1_ls, tr_neg_2_ls, tr_neg_3_ls, tr_neg_4_ls, te_neg_1_ls, te_neg_2_ls, te_neg_3_ls, te_neg_4_ls = neg_data_generate(
@@ -39,7 +40,6 @@ def get_train_val_data(all_data, train_ind, val_ind, adj, seed):
     return train_data_pos, np.array(tr_neg_1_ls), val_data_pos, np.array(val_neg_data)
 
 def get_indep_data(adj, train_data_pos, val_data_pos, seed):
-    """Generate negative samples for training set for independent test"""
     neg_num_test = 30
     train_data_all, tr_neg_1_ls, tr_neg_2_ls, tr_neg_3_ls, tr_neg_4_ls, te_neg_1_ls, te_neg_2_ls, te_neg_3_ls, te_neg_4_ls = neg_data_generate(
         adj, train_data_pos, val_data_pos, neg_num_test, seed)
@@ -53,29 +53,24 @@ def get_indep_data(adj, train_data_pos, val_data_pos, seed):
     return train_data_pos, tr_neg_1_ls, val_data_pos, np.array(val_neg_data)
 
 def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, seed):
-    """Generate negative samples"""
     neg_num_train = 1
     np.random.seed(seed)
     train_neg_1_ls = []; train_neg_2_ls = []; train_neg_3_ls = []; train_neg_4_ls = []
     val_neg_1_ls = []; val_neg_2_ls = []; val_neg_3_ls = []; val_neg_4_ls = []
     arr_true = np.zeros((301, 176, 153))
-    
-    # Mark all positive relationships
+
     for line in adj_data_all:
         arr_true[int(line[0]), int(line[1]), int(line[2])] = 1
-    
-    # Initialize array for training negative samples
+
     arr_false_train = np.zeros((len(set(adj_data_all[:, 0])), len(set(adj_data_all[:, 1])),
                                 len(set(adj_data_all[:, 2]))))
-    
-    # Generate negative samples for training data
+
     for i in train_data_fix:
         ctn_1 = 0; ctn_2 = 0; ctn_3 = 0; ctn_4 = 0
         tr_gene_ls = [j for j in range(0, arr_true.shape[0])]
         tr_mic_ls = [j for j in range(0, arr_true.shape[1])]
         tr_dis_ls = [j for j in range(0, arr_true.shape[2])]
-        
-        # Type 1: Random negative samples (all random)
+
         while ctn_1 < neg_num_train:
             a = np.random.randint(0, arr_true.shape[0] - 1)
             b = np.random.randint(0, arr_true.shape[1] - 1)
@@ -84,24 +79,21 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
                 arr_false_train[a, b, c] = 1
                 ctn_1 += 1
                 train_neg_1_ls.append((a, b, c, 0))
-        
-        # Type 2: Fixed microbe and disease, random gene
+
         while ctn_2 < neg_num_train:
             b = int(i[1]); c = int(i[2]); a = np.random.choice(tr_gene_ls)
             if arr_true[a, b, c] != 1 and arr_false_train[a, b, c] != 1:
                 arr_false_train[a, b, c] = 1
                 ctn_2 += 1
                 train_neg_2_ls.append((a, b, c, 0))
-        
-        # Type 3: Fixed gene and disease, random microbe
+
         while ctn_3 < neg_num_train:
             a = int(i[0]); b = np.random.randint(0, arr_true.shape[1] - 1); c = int(i[2])
             if arr_true[a, b, c] != 1 and arr_false_train[a, b, c] != 1:
                 arr_false_train[a, b, c] = 1
                 ctn_3 += 1
                 train_neg_3_ls.append((a, b, c, 0))
-        
-        # Type 4: Fixed gene and microbe, random disease
+
         while ctn_4 < neg_num_train:
             a = int(i[0]); b = int(i[1])
             if tr_dis_ls != []:
@@ -112,14 +104,12 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
                     ctn_4 += 1
                     train_neg_4_ls.append((a, b, c, 0))
             else:
-                # If no more diseases available, duplicate last sample
                 distance_t4 = neg_num_train - ctn_4
                 last_ind = len(train_neg_4_ls) - 1
                 for k in range(distance_t4):
                     train_neg_4_ls.append(train_neg_4_ls[last_ind])
                 break
 
-    # Combine all training negative samples
     train_neg_1_arr = np.array(train_neg_1_ls)
     train_neg_2_arr = np.array(train_neg_2_ls)
     train_neg_3_arr = np.array(train_neg_3_ls)
@@ -128,24 +118,21 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
         (np.vstack((np.vstack((np.vstack((train_neg_1_arr, train_neg_2_arr)), train_neg_3_arr)), train_neg_4_arr)),
          train_data_fix))
     np.random.shuffle(train_neg_all)
-    
-    # Generate negative samples for validation data
+
     for i in val_data_fix:
         neg_1_i = []; neg_2_i = []; neg_3_i = []; neg_4_i = []
-        # Initialize arrays to prevent duplicates within each positive sample
         arr_false_val_1 = np.zeros((len(set(adj_data_all[:, 0])), len(set(adj_data_all[:, 1])), len(set(adj_data_all[:, 2]))))
         arr_false_val_2 = np.zeros((len(set(adj_data_all[:, 0])), len(set(adj_data_all[:, 1])), len(set(adj_data_all[:, 2]))))
         arr_false_val_3 = np.zeros((len(set(adj_data_all[:, 0])), len(set(adj_data_all[:, 1])), len(set(adj_data_all[:, 2]))))
         arr_false_val_4 = np.zeros((len(set(adj_data_all[:, 0])), len(set(adj_data_all[:, 1])), len(set(adj_data_all[:, 2]))))
-        
+
         neg_1_i.append(i); neg_2_i.append(i); neg_3_i.append(i); neg_4_i.append(i)
         cva_1 = 0; cva_2 = 0; cva_3 = 0; cva_4 = 0
-        
+
         gene_ls = [j for j in range(0, arr_true.shape[0])]
         mic_ls = [j for j in range(0, arr_true.shape[1])]
         dis_ls = [j for j in range(0, arr_true.shape[2])]
-        
-        # Type 1 validation negatives
+
         while cva_1 < neg_num_test:
             a_1 = np.random.randint(0, arr_true.shape[0] - 1)
             b_1 = np.random.randint(0, arr_true.shape[1] - 1)
@@ -156,8 +143,7 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
                 neg_1_i.append((a_1, b_1, c_1, 0))
         np.random.shuffle(neg_1_i)
         val_neg_1_ls.extend(neg_1_i)
-        
-        # Type 2 validation negatives
+
         while cva_2 < neg_num_test:
             b_2 = int(i[1]); c_2 = int(i[2])
             if gene_ls != []:
@@ -168,7 +154,6 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
                     cva_2 += 1
                     neg_2_i.append((a_2, b_2, c_2, 0))
             else:
-                # If no more genes available, duplicate last sample
                 distance_2 = neg_num_test - cva_2
                 last_ind = len(neg_2_i) - 1
                 for k in range(distance_2):
@@ -176,8 +161,7 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
                 break
         np.random.shuffle(neg_2_i)
         val_neg_2_ls.extend(neg_2_i)
-        
-        # Type 3 validation negatives
+
         while cva_3 < neg_num_test:
             a_3 = int(i[0]); c_3 = int(i[2])
             if mic_ls != []:
@@ -195,8 +179,7 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
                 break
         np.random.shuffle(neg_3_i)
         val_neg_3_ls.extend(neg_3_i)
-        
-        # Type 4 validation negatives
+
         while cva_4 < neg_num_test:
             a_4 = int(i[0]); b_4 = int(i[1])
             if dis_ls != []:
@@ -214,47 +197,42 @@ def neg_data_generate(adj_data_all, train_data_fix, val_data_fix, neg_num_test, 
                 break
         np.random.shuffle(neg_4_i)
         val_neg_4_ls.extend(neg_4_i)
-    
+
     return train_neg_all, train_neg_1_ls, train_neg_2_ls, train_neg_3_ls, train_neg_4_ls, val_neg_1_ls, val_neg_2_ls, val_neg_3_ls, val_neg_4_ls
 
 def generate_dataset():
-    """Generate training and test datasets for cross-validation and independent testing"""
     args = parameters_set()
     np.random.seed(args.seed)
-    
-    # Load positive pairs data
+
     adj_data = np.loadtxt('./Data/g_m_d_pos_pairs.txt')
     np.random.shuffle(adj_data)
-    
-    # Split data: 90% for cross-validation, 10% for independent test
+
     cv_data = adj_data[int(0.1 * len(adj_data)):, :]
     indep_data = adj_data[:int(0.1 * len(adj_data)), :]
-    
-    # Generate 5-fold cross-validation datasets
+
     fold_num = 0
     kf = KFold(n_splits=5, shuffle=True, random_state=args.seed)
-    
+
     for train_index, val_index in kf.split(cv_data):
         fold_num += 1
         train_data_pos, train_data_neg, val_data_pos, val_data_neg = get_train_val_data(
             cv_data, train_index, val_index, adj_data, args.seed)
-        
-        # Create directory for fold if it doesn't exist
+
         os.makedirs(f'./Data/CV_data/CV_{fold_num}', exist_ok=True)
-        
-        # Save fold data
+
         np.savetxt(f'./Data/CV_data/CV_{fold_num}/train_data_pos.csv', train_data_pos, delimiter=",", fmt='%d')
         np.savetxt(f'./Data/CV_data/CV_{fold_num}/train_data_neg.csv', train_data_neg, delimiter=',', fmt='%d')
         np.savetxt(f'./Data/CV_data/CV_{fold_num}/val_data_pos.csv', val_data_pos, delimiter=',', fmt='%d')
         np.savetxt(f'./Data/CV_data/CV_{fold_num}/val_data_neg.csv', val_data_neg, delimiter=',', fmt='%d')
-    
-    # Generate independent test dataset
+
     os.makedirs('./Data/indepent_data', exist_ok=True)
     train_data_pos, train_data_neg, test_data_pos, test_data_neg = get_indep_data(
         adj_data, cv_data, indep_data, args.seed)
-    
-    # Save independent test data
+
     np.savetxt('./Data/indepent_data/train_data_pos.csv', train_data_pos, delimiter=",", fmt='%d')
     np.savetxt('./Data/indepent_data/train_data_neg.csv', train_data_neg, delimiter=",", fmt='%d')
     np.savetxt('./Data/indepent_data/test_data_pos.csv', test_data_pos, delimiter=",", fmt='%d')
     np.savetxt('./Data/indepent_data/test_data_neg.csv', test_data_neg, delimiter=",", fmt='%d')
+
+# 避免循环导入，将 parameters_set 放在后面
+from utils import parameters_set
